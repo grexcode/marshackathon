93f0aeba68cbf68d8b5c80f22ec5a1683ffcaa1a
grexcode <79855427+grexcode@users.noreply.github.com>
2022-04-08 08:18:37 -0400

    Create login-page.js

---
+++ login-page.js
@@ -0,0 +1,16 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const password = loginForm.password.value;
+
+    if (password === "june1817") {
+        alert("You have successfully logged in.");
+        location.href = "https://grexcode.github.io/marshackathon/endsolution.html";
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
